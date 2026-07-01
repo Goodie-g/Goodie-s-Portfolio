42f8f4a4f7fc4d95b2cf4dced45b83d134b00866
Goodie <godlucmbise@gmail.com>
2026-07-01 12:49:50 +0300

    feat: update contact icons with links and replace X with Bluesky

--- Goodie's Portfolio/src/sections/Intro.jsx
+++ Goodie's Portfolio/src/sections/Intro.jsx
@@ -2,18 +2,19 @@ import {
   FaGithub,
   FaLinkedin,
   FaInstagram,
-  FaXTwitter
+  FaBluesky
 } from "react-icons/fa6";
 
 import profileImg from "../assets/profile.jpg"
 
 function Intro() {
     const contactIcons = [
-        { name: "Github", icon: <FaGithub /> },
-        { name: "Linked", icon: <FaLinkedin /> },
-        { name: "Instagram", icon: <FaInstagram /> },
-        { name: "X", icon: <FaXTwitter /> }
+        { name: "Github", icon: <FaGithub />, link: "https://github.com/Goodie-g", target: "_blank", rel: "noopener noreferrer" },
+        { name: "Linked", icon: <FaLinkedin />, link: "https://www.linkedin.com/in/godluck-john-mbise-b8aab8323/", target: "_blank", rel: "noopener noreferrer" },
+        { name: "Instagram", icon: <FaInstagram />, link: "https://www.instagram.com/goodie.dev/", target: "_blank", rel: "noopener noreferrer" },
+        { name: "Bluesky", icon: <FaBluesky />, link: "https://bsky.app/profile/goodiedev.bsky.social", target: "_blank", rel: "noopener noreferrer" }
     ]
+
     return (
         <section className="flex justify-start pt-12">
             <div className="flex flex-col items-center">
@@ -24,12 +25,19 @@ function Intro() {
                 <h3 className="text-xl">Self Taught Frontend Developer.</h3>
                 <div className="flex flex-row gap-2 ">
                     { contactIcons.map((contactIcon) => (
-                        <div
-                        key={contactIcon.name}
-                        className="flex flex-col items-center gap-2"
+                        <a 
+                            href={contactIcon.link}
+                            target={contactIcon.target}
+                            rel={contactIcon.rel}
+                            key={contactIcon.name}
+                            className="text-[#58c4dc] transition-all duration-300 hover:text-white hover:scale-110"
                         >
-                        <span className="text-2xl" >{contactIcon.icon}</span>
-                        </div>
+                            <div
+                            className="flex flex-col items-center gap-2"
+                            >
+                            <span className="text-2xl" >{contactIcon.icon}</span>
+                            </div>
+                        </a>
                     ))}
                 </div>
             </div>
