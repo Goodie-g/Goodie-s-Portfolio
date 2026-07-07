c40124f55303bd6e25e82238fdcf056984befc58
Goodie <godlucmbise@gmail.com>
2026-07-07 19:34:36 +0300

    fix: improve styling and responsiveness in About, Header, and Stack components

--- Goodie's Portfolio/src/sections/About.jsx
+++ Goodie's Portfolio/src/sections/About.jsx
@@ -3,6 +3,7 @@ function About() {
         <>
             <h2>About me:</h2>
             <p>I'm a self taught Frontend Developer, with a love for creating tech that pushes society forward.</p>
+            {/* whether it's a quick website fix, a business website, or a full working product, I've got you covered */}
         </>
     )
 }
--- Goodie's Portfolio/src/sections/Header.jsx
+++ Goodie's Portfolio/src/sections/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
     return (
         <header className="flex justify-center">
-            <nav className="flex flex-row gap-2">
-                <HeaderItem value= 'Languages & Tools'/>
+            <nav className="flex flex-row gap-4">
+                <HeaderItem value= 'Languages & Tools' />
                 <HeaderItem value= 'Projects' />
                 <HeaderItem value= 'About'/>
                 <HeaderItem value= 'Contact' />
@@ -13,7 +13,7 @@ const Header = () => {
 
 function HeaderItem({ value }) {
     return (
-        <p className="header-item">{value}</p>
+        <p className="header-item active:scale-95 text-[18px] max-[1024px]:text-[26px] max-[640px]:text-[24px] max-[384px]:text-[22px] hover:scale-110 max-[1024px]:hover:scale-105 transition-all transition duration-200 hover:brightness-125">{value}</p>
     )
 }
 
