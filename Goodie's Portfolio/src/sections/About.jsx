ae34c17aeee8a7935e6408bedcb56c678a41da9f
Goodie <godlucmbise@gmail.com>
2026-08-23 14:10:12 +0300

    fix: update text color and background in CSS; revise About section content

--- Goodie's Portfolio/src/sections/About.jsx
+++ Goodie's Portfolio/src/sections/About.jsx
@@ -2,7 +2,7 @@ function About() {
     return(
         <section id='About'>
             <h2>About me:</h2>
-            <p>I'm a self taught Frontend Developer, with a love for creating tech that pushes society forward.</p>
+            <p>I’m Goodie, a frontend developer and independent web developer based in Tanzania. I enjoy turning ideas into clean, responsive, and practical web experiences. My main focus is frontend development with JavaScript and React, while I’m continuing to expand into backend development and full-stack applications. Alongside personal projects and technical learning, I work directly with clients to design, develop, improve, and launch websites for their businesses and organizations.</p>
            
         </section>
     )
